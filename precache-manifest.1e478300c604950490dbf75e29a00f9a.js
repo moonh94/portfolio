4ca429ac049a620469bed1ae062d3ac64dfc17dd
Hanmi Moon <hmmoon@hotmail.com>
2019-11-06 16:01:15 -0700

    Updates

---
+++ precache-manifest.1e478300c604950490dbf75e29a00f9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbebb2ac7408203bd5b7d18458cfeae",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1f70760e2bed44174402",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8380c6196a255180c8d9",
+    "url": "/portfolio/static/css/main.86a7dbf3.chunk.css"
+  },
+  {
+    "revision": "1f70760e2bed44174402",
+    "url": "/portfolio/static/js/2.df63e456.chunk.js"
+  },
+  {
+    "revision": "8380c6196a255180c8d9",
+    "url": "/portfolio/static/js/main.1a405edb.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c5b45583d8c267045fc82c84736ee5f2",
+    "url": "/portfolio/static/media/profPic.c5b45583.jpg"
+  }
+]);
